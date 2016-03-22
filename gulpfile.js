8f0fecaedcaff8edf4c62d241ca27b10e0043948
DealerOnClass <alberto@ad.dealeron.com>
2016-03-22 15:09:03 -0400

    first update

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+//  Plugins
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync  = require('browser-sync'),
+    cssnano      = require('gulp-cssnano'),
+    htmlmin      = require('gulp-htmlmin'),
+    plumber      = require('gulp-plumber'),
+    sass         = require('gulp-sass'),
+    reload       = browserSync.reload;
+
+//  HTML
+gulp.task('html', function() {
+    gulp.src('./app/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyJS: true
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+//  Fonts
+gulp.task('fonts', function() {
+    gulp.src('./app/fonts/**/*.*')
+        .pipe(gulp.dest('./dist/fonts/'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+//  SASS
+gulp.task('sass', function() {
+    gulp.src('./app/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers:  ['last 2 versions'],
+            cascade:   false
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+//  Serve
+gulp.task('serve', ['sass','html','fonts'], function() {
+    browserSync.init({
+        server: "./dist",
+        notify: false
+    });
+});
+
+//  Watch
+gulp.task('watch', ['serve'], function() {
+    gulp.watch('./app/*.html', ['html']);
+    gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/fonts/**/*.*', ['fonts']);
+});
+
+//  Default
+gulp.task('default', ['watch']);
